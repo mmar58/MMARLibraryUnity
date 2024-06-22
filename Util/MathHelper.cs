f4e708542da50bfc95a4d911bf70d2f67ae50b16
mmar58 <rahmanapu118@gmail.com>
2024-06-22 10:58:48 +0600

    Added MathHelper

---
+++ Util/MathHelper.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathHelper
+{
+    public static float Difference(float x, float y) {
+        return Mathf.Abs(x - y);
+    }
+}
